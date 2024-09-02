f91a2f39bcc09efe8f6ee994ff499a0a03265737
OmkarT8 <omkar.tambe862@gmail.com>
2024-09-02 21:14:01 +0530

    Add project files.

---
+++ CoreApplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CoreApplication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreApplication.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options){}
+        public DbSet<Category> Categories { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Category>().HasData(
+				new Category { ID = 1, Name = "Action", DisplayOrder = 1 },
+				new Category { ID = 2, Name = "Sci-fi", DisplayOrder = 2 },
+				new Category { ID = 3, Name = "Horror", DisplayOrder = 3 }
+			);
+		}
+	}
+}
